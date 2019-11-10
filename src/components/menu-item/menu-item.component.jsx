13eec8190109424384099aaf3635c0932b97dd60
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-11-10 18:26:35 +0100

    Made basic routing

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div className='background-image' style={{
-      backgroundImage: `url(${imageUrl})`
-    }}/>
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
     <div className="content">
       <div className="title">{title.toUpperCase()}</div>
       <span className="subtitle">SHOP NOW</span>
@@ -13,4 +21,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem); //modifies menuItem in this way, that it is possible access to history
